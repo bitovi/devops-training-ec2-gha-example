48ef14b91e3493a18adacaaf972e21142d8c6d10
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-11-16 18:29:29 -0500

    increment demo value 0 (#7)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send("Hello World - from branch: main");
+  res.send("Hello World - test0");
 });
 
 app.listen( port, function () {
