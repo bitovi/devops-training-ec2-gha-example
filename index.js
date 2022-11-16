2748910944aa734d390bbb9359ba46870e5ecbc8
Mick McGrath <mick@bitovi.com>
2022-11-15 19:32:36 -0500

    full example

---
+++ index.js
@@ -0,0 +1,25 @@
+var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+
+const content = () => {
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head>
+      <title>Bitovi DevOps Training - GitHub Action - App to EC2</title>
+  </head>
+  
+  <body>
+      <h1>Hello World</h1>
+  </body>
+  </html>
+  `
+}
+app.get('/', function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen( port, function () {
+  console.log(`Listening on port ${port}!`);
+});
