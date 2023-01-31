70da7b032d8fb2edd465f5bdf5e236433349bd19
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2023-01-31 15:26:15 -0500

    enable postgres code

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-// const { Pool, Client } = require('pg')
+const { Pool, Client } = require('pg')
 
 app.get('/', async function (req, res) {
   console.log("received request", req);
-  // const client = new Client();
-  // const postgresNow = await client.query('SELECT NOW()');
-  // await client.end();
-  res.send(`Hello Bitovi Meetup - github-actions-deploy-docker-to-ec2`)
-//   res.send(`Hello Bitovi Meetup - github-actions-deploy-docker-to-ec2
-// <br />
-// SQL timestamp: ${postgresNow}`);
+  const client = new Client();
+  const postgresNow = await client.query('SELECT NOW()');
+  await client.end();
+  res.send(`Hello SQL timestamp: ${postgresNow}`);
+  // res.send(`Hello Bitovi Meetup - github-actions-deploy-docker-to-ec2`)
   });
 
 app.listen( port, function () {
