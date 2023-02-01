8054ab79633b4b76443f948b26b15a8dbc0e03ef
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2023-02-01 08:03:16 -0500

    dedicated url for postgres

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const { Pool, Client } = require('pg')
 
 app.get('/', async function (req, res) {
   console.log("received request", req.route.path);
+  res.send(`Hello Bitovi Meetup - github-actions-deploy-docker-to-ec2`)
+});
+app.get('/postgres', async function (req, res) {
+  console.log("received request", req.route.path);
   const client = new Client();
   const postgresNow = await client.query('SELECT NOW()');
   await client.end();
   res.send(`Hello SQL timestamp: ${postgresNow}`);
-  // res.send(`Hello Bitovi Meetup - github-actions-deploy-docker-to-ec2`)
-  });
+});
 
 app.listen( port, function () {
   console.log(`Listening on port ${port}!`);
