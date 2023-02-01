a8e865e5bea0ded41aa635ba555100c6b9da86ca
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2023-02-01 17:50:40 -0500

    move client connection to postgres endpoint and specify sql response

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ var fs = require('fs');
 var app = express();
 const { Pool, Client } = require('pg')
 
-const client = new Client({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PGDATABASE,
-  ssl: {
-    ca: fs.readFileSync('rds-combined-ca-bundle.pem').toString()
-  }
-});
-
 app.get('/', async function (req, res) {
   console.log("received request", req.route.path);
   res.send(`Hello Bitovi Meetup - github-actions-deploy-docker-to-ec2`)
 });
 app.get('/postgres', async function (req, res) {
   console.log("received request", req.route.path);
+
+  const client = new Client({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PGDATABASE,
+    ssl: {
+      ca: fs.readFileSync('rds-combined-ca-bundle.pem').toString()
+    }
+  });
   client.connect()
   const result = await client.query('SELECT NOW()');
   await client.end();
   
   
-  res.send(`Hello SQL timestamp: ${result}`);
+  res.send(`Hello SQL timestamp: ${result.rows[0].now}`);
 });
 
 app.listen( port, function () {
