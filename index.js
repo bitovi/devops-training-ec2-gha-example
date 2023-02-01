4edaa54e59d6a8c37ca1ae3e13f055a36464e5fb
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2023-02-01 12:58:25 -0500

    configure client in JS

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ var express = require('express');
 var app = express();
 const { Pool, Client } = require('pg')
 
+
+const client = new Client({
+  host: process.env["PGHOST"],
+  port: process.env["PGPORT"],
+  user: process.env["PG_USER"],
+  password: process.env["PG_PASSWORD"]
+});
+
+
 app.get('/', async function (req, res) {
   console.log("received request", req.route.path);
   res.send(`Hello Bitovi Meetup - github-actions-deploy-docker-to-ec2`)
 });
 app.get('/postgres', async function (req, res) {
   console.log("received request", req.route.path);
-  const client = new Client();
   client.connect()
   const postgresNow = await client.query('SELECT NOW()');
   await client.end();
