7166491938c32bf46ba52fa0e7a5761dedb00053
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2023-02-01 16:54:54 -0500

    use constring connection approach

--- index.js
+++ index.js
@@ -10,18 +10,22 @@ const { Pool, Client } = require('pg')
 //   user: process.env["PG_USER"],
 //   password: process.env["PG_PASSWORD"]
 // });
-const client = new Client({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PGDATABASE,
-  ssl: true
-});
+// const client = new Client({
+//   host: process.env.PGHOST,
+//   port: process.env.PGPORT,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PGDATABASE
+// });
+
+const conStringWithoutCreds = `${process.env.PGHOST}\:${process.env.PGPORT}/${process.env.PG_DATABASE}`
+const conString = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${conStringWithoutCreds}`;
+const client = new Client(conString);
 
 console.log("=========DEBUGGING")
 console.log("pg host", process.env.PGHOST)
 console.log("pg user", process.env.PG_USER)
+console.log("conStringWithoutCreds",conStringWithoutCreds)
 
 
 app.get('/', async function (req, res) {
