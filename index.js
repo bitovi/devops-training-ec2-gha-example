3bbf63d68970137ba29c508e72ef1a02d7cc0dfa
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-10-06 10:52:04 -0300

    Bump

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send("Hello Bitovi Meetup - github-actions-deploy-docker-to-ec2");
+  res.send("Hello world!");
 });
 
 app.listen( port, function () {
