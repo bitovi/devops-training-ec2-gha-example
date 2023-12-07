636ad0831e0c2aaf9c5ba50b00a03866f2521c5f
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-12-07 15:26:56 -0300

    Testing merging deployments

--- sql-files/03.sql
+++ sql-files/03.sql
@@ -7,10 +7,10 @@ create table
         id uuid primary key,
         source text, -- aws:s3, ccd_request, polling_timer
         data_source_id uuid,
-        type text, -- put_object, 
+        type text, -- put_object
         status text, -- completed, open
-        event_date_time timestamptz,
-        completed_date_time timestamptz -- date requested ccd received
+        event_date_time timestamp with time zone,
+        completed_date_time timestamp with time zone -- date requested ccd received
     );
 
 create table
@@ -18,8 +18,8 @@ create table
         id uuid primary key,
         event_id uuid,
         type text, -- file, document, message
-        input_location text, --bucket name
-        input_name text, --object key
+        input_location text, -- bucket name
+        input_name text, -- object key
         archive_bucket text,
         archive_key text
     );
@@ -29,8 +29,8 @@ create table
         id uuid primary key,
         item_id uuid not null,
         step_name text,
-        start_date_time timestamptz,
-        end_date_time timestamptz,
+        start_date_time timestamp with time zone,
+        end_date_time timestamp with time zone,
         step_result text,
         exit_status numeric
     );
@@ -39,7 +39,7 @@ create table
     data_exchange.metric (
         id uuid primary key,
         item_id uuid not null,
-        metric_date_time timestamptz,
+        metric_date_time timestamp with time zone,
         metric_category text,
         metric_name text,
         numeric_value numeric,
@@ -51,17 +51,17 @@ create table
         id uuid primary key,
         item_id uuid not null,
         data_channel_id uuid not null,
-        delivery_date_time timestamptz
+        delivery_date_time timestamp with time zone
     );
 
 create table
     data_exchange.notification (
         id uuid primary key,
-        event_id uuid, -- Notifification might relate to
-        item_id uuid, -- event, item, oe step, so only
+        event_id uuid, -- Notification might relate to
+        item_id uuid, -- event, item, or step, so only
         step_id uuid, -- one of these has a value.
-        issued_date_time timestamptz,
+        issued_date_time timestamp with time zone,
         medium text, -- sms, email, slack
         endpoint text, -- x@y.com, @aschriever
-        message text -- error_message
+        message text -- error message
     );
