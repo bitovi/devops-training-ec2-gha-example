7ae56ea4ad5193ebbceb45ca8ae968341352ac54
LeoDiazL <leo@bitovi.com>
2025-09-23 09:35:23 -0300

    draft

---
+++ infra/ecs/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket  = "${TF_STATE_BUCKET}"
+    region  = "${AWS_REGION}"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
